5346f0018b1a388245d14a4973fa7aea8f02c56c
hedbergj <jhedberg@ccny.cuny.edu>
2018-01-22 13:35:22 -0500

    add hue mapping and adjust ranges

--- two slits/sketch.js
+++ two slits/sketch.js
@@ -8,33 +8,38 @@ var x = 0.0 ;
 var slider;
 
 function setup() {
-createCanvas(800, 800);
+canvas = createCanvas(800, 800);
 noFill();
 
 LightWavelength = createElement('z2', 'Wavelength');
 LightWavelength.position(20, 30);
-slider1 = createSlider(0,1000,10);
+slider1 = createSlider(400,700,10);
 slider1.value(600);
 slider1.position(150, 30);
 
+
 DistanceBTNSlits = createElement('z2', 'Distance between slits');
 DistanceBTNSlits.position(20,55);
-slider2 = createSlider(0,1000,10);
+slider2 = createSlider(10000,50000,100);
 slider2.position(200,55 );
 slider2.value(1000);
 
 SlitWidth = createElement('z2', 'Slit Width');
 SlitWidth.position(20,80);
-slider3 = createSlider(0,1000,10);
+slider3 = createSlider(1,3000,10);
 slider3.position(150, 80);
 slider3.value(50);
 
 CentralMaximum = createElement('z2', 'Central Maximum');
 CentralMaximum.position(20, 105);
-slider4 = createSlider(0,1000,10);
+slider4 = createSlider(0,200,10);
 slider4.position(150, 105);
 slider4.value(140);
  y = new Array(700);
+
+
+ diffraction = createCheckbox('include diffraction', false);
+ diffraction.position(400,80)
 }
 
 function draw() {
@@ -54,45 +59,24 @@ renderFunction();
 
 function calcPlot(){
   for (var x= 0 ; x< y.length; x+=1){
-      theta = map(x,0,y.length,-PI/2,PI/2)
+      theta = map(x,0,y.length,-.1,.1)
+      if(diffraction.checked()==true){
       y[x] = int0 * Math.pow(Math.cos(PI*d*Math.sin(theta)/l),2)*Math.pow(Math.sin(PI*a*Math.sin(theta)/l)/(PI*a*Math.sin(theta)/l),2)
       }
+      else if (diffraction.checked()==false){
+        y[x] = int0 * Math.pow(Math.cos(PI*d*Math.sin(theta)/l),2)
+      }
+    }
+
 }
 function renderFunction() {
 push();
 noFill();
 strokeWeight(4);
-for (var i=0; i<255;i++){
-if (l < 700 && l> 635){
-stroke(255,0,0);
-}
-else if (l>590 && l<635){
-  stroke (204, 102, 0);
-}
-else if (l>560 && l<590){
-  stroke(255, 204, 0);
-}
-else if(l>520 && l<560){
-stroke (0,255,0);
-}
-else if(l>490 && l<520){
-  colorMode(HSB, 100); // Use HSB with scale of 0-100
-//c = color(50, 55, 100);
-stroke(50, 55, 100);
-}
-else if (l>450 && l<490) {
-  stroke(0,0,255);
-}
-else if(l>400 && l<450){
-stroke (175, 100, 220);
-}
-else{
-  stroke(0);
-}}
-//stroke(0);
-//for (var j = 0; j< y.length; j++){
-//  curveVertex(j,height/2-y[j])
-//}
+hue = round(map(l,350,700,330,0));
+c = color('hsb('+hue+', 100%, 100%)');
+stroke(c)
+
 beginShape();
 for (var j = 0; j< y.length; j++){
   curveVertex(j,height/2-y[j])
